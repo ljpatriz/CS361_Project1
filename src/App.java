347f8b22a71024bb01e3599f8e843d8d61def642
Joseph Malionek <jdmalion@colby.edu>
2016-09-08 15:44:53 -0400

    Both buttons now exist and have backgrounds

--- src/App.java
+++ src/App.java
@@ -6,20 +6,30 @@
  * Date: Sept. 6, 2016
  */
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application // VERSION 1
 {
     @Override
     public void start(Stage primaryStage) {
-        Button button = new Button();
-        button.setText("Hello!");
+        Button playButton = new Button();
+        playButton.setText("Play Scale");
+        playButton.setBackground( new Background(new BackgroundFill(Color.LIME, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        Button stopButton = new Button();
+        stopButton.setText("Stop Playing");
+        playButton.setBackground( new Background(new BackgroundFill(Color.PINK, CornerRadii.EMPTY, Insets.EMPTY)));
 
         StackPane root = new StackPane();
-        root.getChildren().add(button);
+        root.getChildren().add(playButton);
 
         Scene scene = new Scene(root, 300, 250);
 
