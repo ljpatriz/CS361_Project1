a96ece33f1e4f9220d93e77fb6492c988ebd75cb
Joseph Malionek <jdmalion@colby.edu>
2016-09-08 16:23:47 -0400

    Buttons adjacent and in Center

--- src/App.java
+++ src/App.java
@@ -7,12 +7,10 @@
  */
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -23,13 +21,19 @@ public class App extends Application // VERSION 1
         Button playButton = new Button();
         playButton.setText("Play Scale");
         playButton.setBackground( new Background(new BackgroundFill(Color.LIME, CornerRadii.EMPTY, Insets.EMPTY)));
+        playButton.setOnAction(event -> System.out.println("playButton clicked"));
 
         Button stopButton = new Button();
         stopButton.setText("Stop Playing");
-        playButton.setBackground( new Background(new BackgroundFill(Color.PINK, CornerRadii.EMPTY, Insets.EMPTY)));
+        stopButton.setBackground( new Background(new BackgroundFill(Color.PINK, CornerRadii.EMPTY, Insets.EMPTY)));
+        stopButton.setOnAction(event -> System.out.println("stopButton clicked"));
 
         StackPane root = new StackPane();
-        root.getChildren().add(playButton);
+        HBox buttonPane = new HBox(5.0);
+        buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.getChildren().addAll(playButton,stopButton);
+        root.getChildren().add(buttonPane);
+
 
         Scene scene = new Scene(root, 300, 250);
 
